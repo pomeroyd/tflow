cd037f9ff096bfb4a5656a7bff1fb8f6e43859b1
David Pomeroy <pomeroyd@tcd.ie>
2017-09-17 18:10:12 +0100

    adding  tflow programs

---
+++ tensor_simple_functions.py
@@ -0,0 +1,24 @@
+# Solution is available in the other "solution.py" tab
+import tensorflow as tf
+
+# TODO: Convert the following to TensorFlow:
+x = 10
+y = 2
+z = x/y - 1
+
+# TODO: Print z from a session
+
+output = None
+x = tf.constant(10)
+y = tf.constant(2)
+#z = tf.constant(tf.float64)
+#a = tf.constant(tf.float64)
+
+a = tf.divide(x,y)
+z = tf.subtract(a,1)
+
+with tf.Session() as sess:
+	output = sess.run(z)
+
+print("output =",output)
+
